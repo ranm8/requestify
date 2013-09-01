4ed8955b9a5c8713d365160f60fe87f6b18be90d
Ran Mizrahi <ranm@codeoasis.com>
2013-09-01 15:46:14 +0300

    (feature) added winston logger

--- test/logger-adapters/winston-adapter-spec.js
+++ test/logger-adapters/winston-adapter-spec.js
@@ -5,9 +5,78 @@
  */
 var mocha = require('mocha'),
     sinon = require('sinon'),
-    expect = require('chai').expect,
-    rewire = require('rewire');
+    chai = require('chai'),
+    expect = chai.expect,
+    rewire = require('rewire'),
+    winstonAdapter = rewire('../../lib/logger-adapters/winston-adapter');
+
+chai.use(require('sinon-chai'));
 
 describe('WinstonAdapter', function() {
 
+    var winstonMock,
+
+        mockMessage = {
+            message: 'hey'
+        },
+
+        adapterInstance;
+
+    describe('configuration', function() {
+        it('should throw an error is an empty object was given', function() {
+            expect(function() {
+                winstonAdapter(null);
+            }).to.throw('winston instance must be set.');
+        });
+    });
+
+    /**
+     * Returns a winston mock object
+     * @returns {{error: *, debug: *, info: *, warning: *}}
+     */
+    function getWinstonMock() {
+        return {
+            error: sinon.stub(),
+            debug: sinon.stub(),
+            info: sinon.stub(),
+            warning: sinon.stub()
+        };
+    }
+
+    beforeEach(function() {
+        winstonMock = getWinstonMock();
+        adapterInstance = winstonAdapter(winstonMock);
+    });
+
+    describe('#error()', function() {
+        it('should call winston.error and log the given message', function() {
+            adapterInstance.error(mockMessage);
+
+            expect(winstonMock.error).have.been.calledWith(mockMessage);
+        });
+    });
+
+    describe('#warning()', function() {
+        it('should call winston.warning and log the given message', function() {
+            adapterInstance.warning(mockMessage);
+
+            expect(winstonMock.warning).have.been.calledWith(mockMessage);
+        });
+    });
+
+    describe('#debug()', function() {
+        it('should call winston.debug and log the given message', function() {
+            adapterInstance.debug(mockMessage);
+
+            expect(winstonMock.debug).have.been.calledWith(mockMessage);
+        });
+    });
+
+    describe('#info()', function() {
+        it('should call winston.info and log the given message', function() {
+            adapterInstance.info(mockMessage);
+
+            expect(winstonMock.info).have.been.calledWith(mockMessage);
+        });
+    });
 });
